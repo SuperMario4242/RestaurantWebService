631bb69e9250f186b30f3fa4302af205e151b347
SuperMario4242 <36709496+SuperMario4242@users.noreply.github.com>
2024-03-20 08:32:26 +0200

    new commit
    commit

--- src/main/java/com/webservice/restaurant/SampleDataLoader.java
+++ src/main/java/com/webservice/restaurant/SampleDataLoader.java
@@ -5,6 +5,9 @@ import com.webservice.restaurant.Entities.Restaurant;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Component
 public class SampleDataLoader implements CommandLineRunner {
@@ -20,45 +23,70 @@ public class SampleDataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        List<Dish> dishList = new ArrayList<Dish>();
         // Pridedame pavyzdinius restoranus
         Restaurant restaurantA = new Restaurant();
         restaurantA.setName("Restaurant A");
+        restaurantA.setAddress("Gedimino Pr.");
+        restaurantA.setCuisine("Italian");
+        restaurantA.setDishes(dishList);
         restaurantService.createRestaurant(restaurantA);
 
         Restaurant restaurantB = new Restaurant();
         restaurantB.setName("Restaurant B");
+        restaurantB.setAddress("Vokieciu gatve.");
+        restaurantB.setCuisine("German");
+        restaurantB.setDishes(dishList);
         restaurantService.createRestaurant(restaurantB);
 
         Restaurant restaurantC = new Restaurant();
         restaurantC.setName("Restaurant C");
+        restaurantC.setAddress("Kalvarijos gatve.");
+        restaurantC.setCuisine("Polish");
+        restaurantC.setDishes(dishList);
         restaurantService.createRestaurant(restaurantC);
 
         // Pridedame pavyzdinius patiekalus
         Dish dish1 = new Dish();
         dish1.setTitle("Dish 1");
         dish1.setPrice(10.99f);
+        dish1.setCuisine("Italian");
+        dish1.setPotionSize(2);
         dish1.setRestaurant(restaurantA);
         restaurantService.createDish(dish1);
 
         Dish dish2 = new Dish();
+
+
+        dish2.setCuisine("Italian");
+        dish2.setPotionSize(2);
         dish2.setTitle("Dish 2");
         dish2.setPrice(15.99f);
         dish2.setRestaurant(restaurantA);
         restaurantService.createDish(dish2);
 
         Dish dish3 = new Dish();
+        dish3.setCuisine("German");
+        dish3.setPotionSize(2);
         dish3.setTitle("Dish 3");
         dish3.setPrice(8.99f);
         dish3.setRestaurant(restaurantB);
         restaurantService.createDish(dish3);
 
         Dish dish4 = new Dish();
+
+        dish4.setCuisine("Polish");
+        dish4.setPotionSize(1);
+
         dish4.setTitle("Dish 4");
         dish4.setPrice(12.50f);
         dish4.setRestaurant(restaurantC);
         restaurantService.createDish(dish4);
 
         Dish dish5 = new Dish();
+        dish1.setCuisine("Polish");
+        dish1.setPotionSize(2);
         dish5.setTitle("Dish 5");
         dish5.setPrice(9.75f);
         dish5.setRestaurant(restaurantC);
